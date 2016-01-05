7e0df0761e2b0d57a2c47023bf1780a8b3b91bec
Lionel DEMANOU <lionel.demanou@compareagences.com>
2016-01-05 16:47:02 +0100

    modifier

--- agent.js
+++ agent.js
@@ -24,7 +24,7 @@ var mailer = require('../mailer/mail');
 var Slack = require('node-slack')
 var slack = new Slack('https://hooks.slack.com/services/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');
 
-lionel
+test3 changed
 
 exports.agent = function (req, res, next, id) {
 
